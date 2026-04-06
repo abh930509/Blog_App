521285f638206f6a7493953f78e54b1f205c97e9
Abhishek Giri <148493724+abh930509@users.noreply.github.com>
2026-04-06 10:40:24 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,16 +14,14 @@ const app  = express();
 
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "http://localhost:8081",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
 
 
 
-app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

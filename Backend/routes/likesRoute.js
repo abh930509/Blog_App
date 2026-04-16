fc08ceeb1207d1eaaabdd33ee8e78286d744f44b
Abhishek Giri <148493724+abh930509@users.noreply.github.com>
2026-04-16 11:55:18 +0530

    Add GetLikesController to likesRoute
    Added GetLikesController to handle fetching likes for a post.

--- Backend/routes/likesRoute.js
+++ Backend/routes/likesRoute.js
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { ToggleLikeController,getLikesController,myLikedPosts } from '../controllers/likesController.js';
+import { ToggleLikeController,getLikesController,myLikedPosts,GetLikesController } from '../controllers/likesController.js';
 import auth from '../middleware/auth.js';
 const likeRouter =Router();
 
-likeRouter.post("/like/:postId" ,auth,ToggleLikeController); // likes and unlike toggle route
+likeRouter.post("/like/:postId" ,auth,ToggleLikeController);
+likeRouter.get("/liked/:postId",GetLikesController); // likes and unlike toggle route
 likeRouter.get("/likes/:postId" ,getLikesController);  // get total likes on post
 likeRouter.get("/mylikedposts",auth,myLikedPosts);  // get all my liked posts
 
